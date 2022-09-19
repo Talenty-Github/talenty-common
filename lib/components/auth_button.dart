2442ea1b1152e4f9d6f840a3b4a295e593691e8b
Rogelio <rogelio.garcia.escallon@gmail.com>
2022-09-19 17:38:39 -0500

    Change context menu for popup menu (#2)
    Co-authored-by: Rogelio Garcia Escallon <rogelio.garciae@perficient.com>

--- lib/components/auth_button.dart
+++ lib/components/auth_button.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:talenty_common/components/popupmenu.dart';
 import 'package:talenty_common/fundamentals/colors.dart';
-import 'package:talenty_common/components/context_menu.dart';
 import 'package:talenty_common/components/icons.dart';
 
 class TalentyAuthButton extends StatefulWidget {
-  final VoidCallback onTapRegister;
-  final VoidCallback onTapLogin;
   final Image? profilePicture;
+  final List<TalentyPopupMenuItem> menuItems;
 
   const TalentyAuthButton({
-    required this.onTapRegister,
-    required this.onTapLogin,
+    required this.menuItems,
     this.profilePicture,
     super.key,
   });
@@ -24,74 +22,52 @@ class _TalentyAuthButtonState extends State<TalentyAuthButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 16, bottom: 16),
-      child: Material(
-        elevation: _authPressed ? 7 : 0,
-        color: TalentyColors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: TalentyColors.carbon200,
-            ),
-            borderRadius: const BorderRadius.all(Radius.circular(24)),
-          ),
-          height: 48,
-          width: 96,
-          child: InkWell(
-            onTap: () {
-              setState(() {
-                _authPressed = !_authPressed;
-              });
-              showDialog(
-                  context: context,
-                  barrierColor: Colors.white.withOpacity(0),
-                  builder: (BuildContext context) {
-                    return TalentyContextMenu(
-                      options: [
-                        ContextMenuOption(
-                          text: 'Regístrate',
-                          callback: widget.onTapRegister,
-                        ),
-                        ContextMenuOption(
-                          text: 'Inicia sesión',
-                          callback: widget.onTapLogin,
-                        )
-                      ],
-                    );
-                  }).then((_) {
-                setState(() {
-                  _authPressed = !_authPressed;
-                });
-              });
-            },
-            borderRadius: const BorderRadius.all(Radius.circular(24)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TalentyIcon(
-                  icon: TalentyIcons.navigation.burgerMenu,
-                  size: TalentyIconSize.medium,
-                  color: TalentyColors.carbon600,
+    return InkWell(
+        onTap: () => setState(() {
+              _authPressed = !_authPressed;
+            }),
+        child: TalentyPopupMenu(
+          menuItems: widget.menuItems,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, bottom: 16),
+            child: Material(
+              elevation: _authPressed ? 7 : 0,
+              color: TalentyColors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: TalentyColors.carbon200,
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(24)),
                 ),
-                const SizedBox(width: 6),
-                CircleAvatar(
-                  radius: 16.0,
-                  backgroundColor: TalentyColors.carbon600,
-                  backgroundImage: widget.profilePicture?.image,
-                  child: TalentyIcon(
-                      icon: TalentyIcons.essentials.user,
-                      size: TalentyIconSize.small,
-                      color: TalentyColors.white),
+                height: 48,
+                width: 96,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TalentyIcon(
+                      icon: TalentyIcons.navigation.burgerMenu,
+                      size: TalentyIconSize.medium,
+                      color: TalentyColors.carbon600,
+                    ),
+                    const SizedBox(width: 6),
+                    CircleAvatar(
+                      radius: 16.0,
+                      backgroundColor: TalentyColors.carbon600,
+                      backgroundImage: widget.profilePicture?.image,
+                      child: TalentyIcon(
+                          icon: TalentyIcons.essentials.user,
+                          size: TalentyIconSize.small,
+                          color: TalentyColors.white),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
