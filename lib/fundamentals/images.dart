26f7c6af43b3b0c0ef8401bb25957b600109509e
Keiler Guardo <kguardo@talenty.com.co>
2022-09-22 16:48:36 -0500

    history photo added

--- lib/fundamentals/images.dart
+++ lib/fundamentals/images.dart
@@ -4,4 +4,7 @@ class TalentyImages {
   static const String talentySteps = '$_imagesPath/talenty-steps.png';
   static const String login = '$_imagesPath/login.png';
   static const String providersImage = '$_imagesPath/providers-image.png';
+  
+  // history photos
+  static List<String> historyPhotos = ['$_imagesPath/storyphoto01.jpg'];
 }
