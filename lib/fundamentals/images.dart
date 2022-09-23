5d3e48029b12f9e07048f9d0ce697fb87135abb9
kguardo21 <kguardo@talenty.com.co>
2022-09-23 12:50:30 -0500

    photos banners added

--- lib/fundamentals/images.dart
+++ lib/fundamentals/images.dart
@@ -4,6 +4,9 @@ class TalentyImages {
   static const String talentySteps = '$_imagesPath/talenty-steps.png';
   static const String login = '$_imagesPath/login.png';
   static const String providersImage = '$_imagesPath/providers-image.png';
+  static const String bannerTalent = '$_imagesPath/banner-talentos.png';
+  static const String bannerCompany = '$_imagesPath/banner-empresas.png';
+  static const String bannerCelebrity = '$_imagesPath/banner-celebrity.png';
   
   // history photos
   static List<String> historyPhotos = ['$_imagesPath/storyphoto01.jpg'];
