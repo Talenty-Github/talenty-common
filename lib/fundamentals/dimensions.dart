72dbab6698f9d9e896a0c56f0fb3668153f87a07
kguardo21 <kguardo@talenty.com.co>
2022-10-01 16:40:19 -0500

    talenty dimensions

---
+++ lib/fundamentals/dimensions.dart
@@ -0,0 +1,9 @@
+class TalentyDimension {
+  static double desktopBreakpoint = 1025;
+  static double tabletBreakpoint = 600;
+  static double mobileBreakpoint = 0;
+
+  static double desktopMaxWidth = 1168;
+  static double desktopPadding = 16.0;
+  static double tableetPadidng = 16.0;
+}
