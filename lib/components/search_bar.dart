e338db6db1b18cd67ba6f440d55ff76a2cda47a1
Nicolás Hernández <nm.hernandez1996@gmail.com>
2022-10-05 11:35:25 -0500

    search bar

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:talenty_common/components/icons.dart';
 import 'package:talenty_common/fundamentals/colors.dart';
 import 'package:talenty_common/fundamentals/gap_spaces.dart';
@@ -12,19 +13,16 @@ class TalentySearchBar extends StatelessWidget {
     const double borderRadius = 24.0;
 
     return Container(
-      width: 405,
+      constraints: BoxConstraints(
+        maxWidth: getValueForScreenType<double>(context: context, mobile: 406.0 * 1.5, tablet: 406.0, desktop: 406.0),
+      ),
       height: 48,
       padding: EdgeInsets.symmetric(horizontal: TalentyGapSpaces.level4),
       decoration: BoxDecoration(
         color: TalentyColors.white,
         border: Border.all(color: TalentyColors.carbon200),
         borderRadius: BorderRadius.circular(borderRadius),
-        boxShadow: const [
-          BoxShadow(
-              color: TalentyColors.carbon200,
-              blurRadius: 4,
-              offset: Offset(0, 4))
-        ],
+        boxShadow: const [BoxShadow(color: TalentyColors.carbon200, blurRadius: 4, offset: Offset(0, 4))],
       ),
       child: TextFormField(
         decoration: InputDecoration(
@@ -33,8 +31,7 @@ class TalentySearchBar extends StatelessWidget {
             color: TalentyColors.carbon600,
           ),
           border: InputBorder.none,
-          suffixIconConstraints: const BoxConstraints(
-              minHeight: 24, minWidth: 24, maxHeight: 24, maxWidth: 24),
+          suffixIconConstraints: const BoxConstraints(minHeight: 24, minWidth: 24, maxHeight: 24, maxWidth: 24),
           suffixIcon: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(borderRadius),
@@ -43,9 +40,7 @@ class TalentySearchBar extends StatelessWidget {
             padding: EdgeInsets.all(TalentyGapSpaces.level1),
             child: SizedBox(
               child: TalentyIcon(
-                  icon: TalentyIcons.actions.search,
-                  size: TalentyIconSize.small,
-                  color: TalentyColors.white),
+                  icon: TalentyIcons.actions.search, size: TalentyIconSize.small, color: TalentyColors.white),
             ),
           ),
         ),
--- lib/fundamentals/dimensions.dart
+++ lib/fundamentals/dimensions.dart
@@ -4,7 +4,8 @@ class TalentyDimension {
   static double mobileBreakpoint = 0;
 
   static double desktopMaxWidth = 1168;
-  
+
+  static double mobilePadding = 16.0;
   static double desktopPadding = 16.0;
   static double tabletPadidng = 16.0;
 }
